36632d7c78967df039606daf72e2ef937e53219a
habibkoe <habibsantri@gmail.com>
2023-12-27 08:33:21 +0800

    update some update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,4 @@
 import { authOptions } from "@/libs/authOptions";
 import NextAuth from "next-auth/next";
 
-// const handler = NextAuth(authOptions)
-
-
-// export {handler as GET, handler as POST}
-
 export default NextAuth(authOptions);
